f236cbcbed505b704a9b19121f0139279d99b16c
ProAndroid <zxn18789469611@gmail.com>
2015-09-30 22:52:19 +0800

    modify HttpUrlConnStack

--- zxn_http/src/main/java/com/zxn/zxn_http/httpstacks/HttpUrlConnStack.java
+++ zxn_http/src/main/java/com/zxn/zxn_http/httpstacks/HttpUrlConnStack.java
@@ -4,10 +4,12 @@ import com.zxn.zxn_http.base.Request;
 import com.zxn.zxn_http.base.Response;
 import com.zxn.zxn_http.config.HttpUrlConnConfig;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Set;
 
 /**
  * 使用HttpURLConnection执行网络请求的HttpStack
@@ -36,11 +38,20 @@ public class HttpUrlConnStack implements HttpStack {
             httpURLConnection = createUrlConnection(request.getUrl());
             // 设置Headers
             setRequestHeaders(httpURLConnection, request);
+            // 设置body参数
+            setRequestParams(httpURLConnection, request);
         }
 
         return null;
     }
 
+    /**
+     * 创建HttpURLConnection
+     *
+     * @param url
+     * @return
+     * @throws IOException
+     */
     private HttpURLConnection createUrlConnection(String url) throws IOException {
         URL newUrl = new URL(url);
         URLConnection urlConnection = newUrl.openConnection();
@@ -50,4 +61,42 @@ public class HttpUrlConnStack implements HttpStack {
         urlConnection.setUseCaches(false);
         return (HttpURLConnection) urlConnection;
     }
+
+    /**
+     * 设置请求头
+     *
+     * @param connection
+     * @param request
+     */
+    private void setRequestHeaders(HttpURLConnection connection, Request<?> request) {
+        Set<String> headersKeys = request.getHeaders().keySet();
+        for (String headerName : headersKeys) {
+            connection.addRequestProperty(headerName, request.getHeaders().get(headerName));
+        }
+    }
+
+    /**
+     * 设置请求参数
+     *
+     * @param connection
+     * @param request
+     * @throws IOException
+     */
+    private void setRequestParams(HttpURLConnection connection, Request<?> request) throws IOException {
+        Request.HttpMethod method = request.getHttpMethod();
+        connection.setRequestMethod(method.toString());
+        // add params
+        byte[] body = request.getBody();
+        if (body != null) {
+            // enable output
+            connection.setDoOutput(true);
+            // set content type
+            connection.addRequestProperty(Request.HEADER_CONTENT_TYPE, request.getBodyContentType());
+            // write params data to connection
+            DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
+            dataOutputStream.write(body);
+            dataOutputStream.close();
+        }
+    }
+
 }
